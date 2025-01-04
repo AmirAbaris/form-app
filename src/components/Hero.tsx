38080a62071ad3c130a7cf0ba2335e1c8b36bda1
Amir Abaris <thisisamirabaris@gmail.com>
2025-01-04 12:08:52 +0330

    refactored to be TS vite and moved codes here

---
+++ src/components/Hero.tsx
@@ -0,0 +1,16 @@
+export default function Hero() {
+  return (
+    <div className="mx-auto">
+      <h1 className="font-extrabold text-3xl pt-24 md:text-5xl lg:text-7xl">
+        Welcome to FormFlow
+      </h1>
+      <div className="rounded-3xl mt-12">
+        <img
+          src="../../assets/images/cc1.jpg"
+          alt="hero cover"
+          className="rounded-3xl aspect-video max-h-[800px] mx-auto"
+        />
+      </div>
+    </div>
+  );
+}
