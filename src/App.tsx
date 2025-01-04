50996a7683f3833992190e7950bc1d26d1f36efa
Amir Abaris <thisisamirabaris@gmail.com>
2025-01-04 12:22:37 +0330

    added cta component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Cta from "./components/Cta";
 import Hero from "./components/Hero";
 
 function App() {
   return (
-    <main className="container mx-auto px-12 py-12 dark">
-      <Hero />
+    <main className="space-y-24">
+      <section className="container mx-auto px-12 py-12 space-y-12">
+        <Hero />
+      </section>
+      <Cta />
     </main>
   );
 }
